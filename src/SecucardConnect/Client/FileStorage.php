de994f70e619eba8e41ba1475ab1edd6b60d9f0a
tkrau55 <thomas.krauss@posteo.de>
2015-12-17 13:21:29 +0100

    Update: Enable  stream caching  with file storage.

--- src/SecucardConnect/Client/FileStorage.php
+++ src/SecucardConnect/Client/FileStorage.php
@@ -2,30 +2,50 @@
 
 namespace SecucardConnect\Client;
 
-class FileStorage extends DummyStorage {
-    
-    public function __construct($file) {
-        // init dummy storage var
-        $this->storage = array();
-        
-        $this->file = $file;
+use Psr\Http\Message\StreamInterface;
+
+class FileStorage extends DummyStorage
+{
+    private $dir;
+
+    public function __construct($dir)
+    {
+        parent::__construct();
+
+        if (is_file($dir)) {
+            throw new ClientError('Invalid storage location, file with same name already exists.');
+        }
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        $this->dir = $dir;
+        $this->load();
     }
-    
+
     /**
      * Retrieve an item from cache
      *
      * @param string $key The key to store it under
      * @return mixed
      */
-    public function get($key) {
-        $this->load();
-        
+    public function get($key)
+    {
         if (isset($this->storage[$key])) {
             return $this->storage[$key];
         }
-        
-        return false;
+
+        $file = $this->findFile($key);
+        if ($file === false) {
+            return false;
+        }
+
+        if ($file === 0) {
+            return null;
+        }
+
+        return fopen($file, 'r');
     }
+
 
     /**
      * Set an item in the cache
@@ -34,11 +54,27 @@ class FileStorage extends DummyStorage {
      * @param mixed $value
      * @return bool
      */
-    public function set($key, $value) {
-        $this->load();
-        $this->storage[$key] = $value;
-        return $this->save();
+    public function set($key, $value)
+    {
+        // take care about replacing keys !
+        if (is_resource($value) || $value instanceof StreamInterface) {
+            $res = file_put_contents($this->filePath($key), $value);
+            if ($res !== false) {
+                // clear plain storage from key
+                return $this->deleteStore($key);
+            }
+            return $res;
+        } else {
+            $this->storage[$key] = $value;
+            $res = $this->save();
+            if ($res !== false) {
+                // clear file storage from key
+                return $this->deleteFile($key);
+            }
+            return $res;
+        }
     }
+
 
     /**
      * Remove a key from the cache.
@@ -46,10 +82,14 @@ class FileStorage extends DummyStorage {
      * @param string $key
      * @return bool
      */
-    public function delete($key) {
-        $this->load();
-        unset($this->storage[$key]);
-        return $this->save();
+    public function delete($key)
+    {
+        $res = $this->deleteFile($key);
+        if ($res === false) {
+            return false;
+        }
+
+        return $this->deleteStore($key);
     }
 
     /**
@@ -57,24 +97,86 @@ class FileStorage extends DummyStorage {
      *
      * @return bool
      */
-    public function deleteAll() {
-        $this->load();
-        $this->storage = array();
-        return $this->save();
-    }
-    
-    
-    private function load() {
-        $data = @file_get_contents($this->file);
-        if ($data) {
-            $this->storage = json_decode($data, TRUE);
-            return true;
+    public function deleteAll()
+    {
+        try {
+            array_map('unlink', glob($this->filePath('*')));
+        } catch (\Exception $e) {
+            return false;
         }
-       return false;
-    }
-    
-    private function save() {
-        file_put_contents($this->file, json_encode($this->storage));
+        $this->storage = array();
         return true;
     }
-} 
+
+    private function load()
+    {
+        $filename = $this->filePath();
+        $data = @file_get_contents($filename);
+        if ($data) {
+            $this->storage = json_decode($data, true);
+            return true;
+        }
+        return false;
+    }
+
+    private function save()
+    {
+        file_put_contents($this->filePath(), json_encode($this->storage));
+        return true;
+    }
+
+
+    private function findFile($key)
+    {
+        $files = glob($this->filePath($key));
+        if ($files === false) {
+            return false;
+        }
+
+        $c = count($files);
+        if ($c === 0) {
+            return 0;
+        }
+
+        if ($c === 1) {
+            return $files[0];
+        }
+
+        // to much files found, error, should not happen...
+        return false;
+    }
+
+    private function deleteFile($key)
+    {
+        $file = $this->findFile($key);
+        if ($file === false) {
+            return false; // error
+        }
+
+        if ($file === 0) {
+            return true; // nothing found
+        }
+
+        return unlink($file);
+    }
+
+    private function deleteStore($key)
+    {
+        if (isset($this->storage[$key])) {
+            unset($this->storage[$key]);
+            return $this->save();
+        }
+
+        return true;
+    }
+
+    /**
+     * @param null $file
+     * @return string
+     */
+    private function filePath($file = null)
+    {
+        // use  default name for json data
+        return $this->dir . DIRECTORY_SEPARATOR . ($file == null ? 'YmFuYW5lDQo' : $file);
+    }
+}
