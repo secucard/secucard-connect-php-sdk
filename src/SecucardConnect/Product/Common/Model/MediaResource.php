e066fdaadfe5f32cab6774b6b7383207afe3f9db
tkrau55 <thomas.krauss@posteo.de>
2015-12-18 16:14:32 +0100

    New: Add media resource support for download

---
+++ src/SecucardConnect/Product/Common/Model/MediaResource.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace SecucardConnect\Product\Common\Model;
+
+use SecucardConnect\Client\MediaResourceTrait;
+
+
+/**
+ * Common (base) class modeling URL based media resources like images or pdf documents.
+ * @see \SecucardConnect\Client\MediaResourceTrait
+ * @package SecucardConnect\Product\Common\Model
+ */
+class MediaResource
+{
+    use MediaResourceTrait;
+}
