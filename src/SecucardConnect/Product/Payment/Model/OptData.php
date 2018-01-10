bafd6f269d32913573a11f516978c46ebd7269e2
Rico Simlinger <r.simlinger@secupay.ag>
2018-01-10 12:26:41 +0100

    ADD payment iframe options

--- src/SecucardConnect/Product/Payment/Model/OptData.php
+++ src/SecucardConnect/Product/Payment/Model/OptData.php
@@ -5,8 +5,7 @@ namespace SecucardConnect\Product\Payment\Model;
 use SecucardConnect\Product\Common\Model\BaseModel;
 
 /**
- * RedirectUrl Data Model class
- *
+ * OptData Data Model class
  */
 class OptData extends BaseModel
 {
@@ -19,4 +18,58 @@ class OptData extends BaseModel
      * @var bool
      */
     public $hide_disclaimer = false;
+
+    /**
+     * Define the user language of the payment iframe
+     * Possible values are:
+     * - "en_US"
+     * - "de_DE"
+     * - "nl_NL"
+     * - "fr_FR"
+     * - "it_IT"
+     * - "ru_UA"
+     * - "ru_RU"
+     *
+     * @var string
+     */
+    public $language = 'de_DE';
+
+    /**
+     * The label of the headline above the basket
+     * Default for DE: "Rechnungsübersicht", for EN: "Overview"
+     *
+     * @var string
+     */
+    public $basket_title;
+
+    /**
+     * The label of the payment hint
+     * Default for DE: "Hinweis", for EN: "Hint"
+     *
+     * @var string
+     */
+    public $payment_hint_title;
+
+    /**
+     * The label of the submit button
+     * Default for DE: "Kauf bestätigen", for EN: "Submit Payment"
+     *
+     * @var string
+     */
+    public $submit_button_title;
+
+    /**
+     * The label of the cancel button
+     * Default for DE: "abbrechen", for EN: "Cancel Payment"
+     *
+     * @var string
+     */
+    public $cancel_button_title;
+
+    /**
+     * The name of the project
+     *
+     * @var string
+     */
+    public $project_title;
 }
