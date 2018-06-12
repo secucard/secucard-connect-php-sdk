5b968f169755183bc28c926b82900c46c87b071f
Rico Simlinger <r.simlinger@secupay.ag>
2018-06-12 10:21:51 +0200

    Add missing phpdoc

--- src/SecucardConnect/Auth/ClientCredentials.php
+++ src/SecucardConnect/Auth/ClientCredentials.php
@@ -7,13 +7,20 @@ namespace SecucardConnect\Auth;
  */
 class ClientCredentials implements GrantTypeInterface
 {
+    /**
+     * @var string
+     */
     public $client_id;
+    
+    /**
+     * @var string
+     */
     public $client_secret;
 
     /**
      * Constructor
-     * @param $clientId
-     * @param $clientSecret
+     * @param string $clientId
+     * @param string $clientSecret
      */
     public function __construct($clientId, $clientSecret)
     {
@@ -21,6 +28,9 @@ class ClientCredentials implements GrantTypeInterface
         $this->client_secret = $clientSecret;
     }
 
+    /**
+     * @return string
+     */
     public function getType()
     {
         return 'client_credentials';
