0f0b2230c90d5e22cf8b774f8a0eabaf728e04b9
Wojciech Proma <w.proma@secupay.ag>
2018-09-05 18:00:08 +0200

    Adding new method to general contract service to get available payment methods

--- src/SecucardConnect/Product/General/ContractsService.php
+++ src/SecucardConnect/Product/General/ContractsService.php
@@ -3,6 +3,7 @@
 namespace SecucardConnect\Product\General;
 
 use SecucardConnect\Client\ProductService;
+use SecucardConnect\Product\General\Model\PaymentMethodsRequestParams;
 
 /**
  * Provides operations for General/Contracts product.
@@ -10,5 +11,15 @@ use SecucardConnect\Client\ProductService;
  */
 class ContractsService extends ProductService
 {
-    
+    /**
+     * Retrieves available payment methods for given contract
+     *
+     * @param PaymentMethodsRequestParams $param
+     * @param string $contract_id
+     * @return string[]
+     */
+    public function getAvailablePaymentMethods(PaymentMethodsRequestParams $param, $contract_id)
+    {
+        return $this->execute($contract_id, 'getAvailablePaymentMethods', null, $param);
+    }
 }
---
+++ src/SecucardConnect/Product/General/Model/PaymentMethodsRequestParams.php
@@ -0,0 +1,30 @@
+<?php
+namespace SecucardConnect\Product\General\Model;
+use SecucardConnect\Product\Common\Model\BaseModel;
+
+/**
+ * PaymentMethodsRequestParams Api Model class
+ */
+class PaymentMethodsRequestParams extends BaseModel
+{
+    /**
+    * PaymentMethodsRequestParams constructor.
+    * @param int $is_demo
+    * @param string $currency
+    */
+    public function __construct($is_demo, $currency)
+    {
+        $this->is_demo = $is_demo;
+        $this->currency = $currency;
+    }
+
+    /**
+    * @var int
+    */
+    public $is_demo;
+
+    /**
+    * @var string
+    */
+    public $currency;
+}
