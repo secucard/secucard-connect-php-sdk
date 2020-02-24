51a487e7a08ed38f13b4732e9e2c237751eb9e3b
Anton Lunyov <anton.lunyov@gmail.com>
2020-02-24 14:06:30 +0100

    SCCORE-3464, add new method getPaymentWizardOptions()

--- src/SecucardConnect/Product/General/ContractsService.php
+++ src/SecucardConnect/Product/General/ContractsService.php
@@ -8,6 +8,7 @@ use SecucardConnect\Client\AuthError;
 use SecucardConnect\Client\ClientError;
 use SecucardConnect\Client\ProductService;
 use SecucardConnect\Product\General\Model\PaymentMethodsRequestParams;
+use SecucardConnect\Product\Smart\Model\PaymentWizardContractOptions;
 
 /**
  * Provides operations for General/Contracts product.
@@ -29,5 +30,20 @@ class ContractsService extends ProductService
     public function getAvailablePaymentMethods(PaymentMethodsRequestParams $param, $contract_id)
     {
         return $this->execute($contract_id, 'getAvailablePaymentMethods', null, $param);
+    }
+
+    /**
+     * Retrieves Payment Wizard options assigned to this contract
+     *
+     * @param string $contract_id
+     * @return bool|mixed|null|string
+     * @throws ApiError
+     * @throws AuthError
+     * @throws ClientError
+     * @throws GuzzleException
+     */
+    public function getPaymentWizardOptions($contract_id)
+    {
+        return $this->getWithAction($contract_id, 'iframeOptions', null, null, PaymentWizardContractOptions::class);
     }
 }
